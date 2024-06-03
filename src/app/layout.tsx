f2f44c5093288d32ac827331be577b3ac84c5b00
mitenkumar <mitup7869@gmail.com>
2024-06-04 00:07:54 +0530

    feat : add metadata for  image and url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,11 @@ import { NotificationProvider } from "@/components/provider/NotificationContext"
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Miten | Full Stack Web Developer",
   description: "Explore the portfolio of Miten Patel, a skilled Full Stack Developer specializing in modern web application development. Discover projects, skills, and get in touch.",
+  imageUrl: "https://i.ibb.co/QkXzsRr/Screenshot-2024-06-04-000548.png", 
+  canonicalUrl: "https://my-portfolio-kappa-bay.vercel.app/",
 };
 
 export default function RootLayout({
