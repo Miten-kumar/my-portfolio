760ee1ea4cf1e979ea635fc47e7724d493841d99
mitenkumar <mitup7869@gmail.com>
2024-06-19 01:35:32 +0530

    feat  : set og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <meta name="description" content="Explore the portfolio of Miten Patel, a skilled Full Stack Developer specializing in modern web application development. Discover projects, skills, and get in touch." />
       <meta property="og:title" content="Miten | Full Stack Web Developer" />
       <meta property="og:description" content="Explore the portfolio of Miten Patel, a skilled Full Stack Developer specializing in modern web application development. Discover projects, skills, and get in touch." />
-      <meta property="og:image" content='https://i.ibb.co/h7cdKKM/IMG-20240606-190834.jpg' key="og_image_global" />
+      <meta property="og:image" content='https://i.ibb.co/B3vqkrr/Screenshot-2024-06-19-013106.png' key="og_image_global" />
       <meta property="og:url" content="https://my-portfolio-kappa-bay.vercel.app/" />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
